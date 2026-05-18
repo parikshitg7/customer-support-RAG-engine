2a88fac976fb4c1afb94413927ce7f0b4b92a601
parikshitg7 <parikshitupadhyay336@gmail.com>
2026-05-18 22:52:26 +0530

    replaced langchain with langchain_classic

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq 
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # 1. Setup the Page
